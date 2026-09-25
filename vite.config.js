ca5c016e8edc0f849c0b79e28ae5d10b1aea2219
berrylmondow <berrylmondow@gmail.com>
2026-09-25 22:13:42 +0700

    setup github pages

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), vueDevTools()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
+
   base: '/masjidKu/',
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
